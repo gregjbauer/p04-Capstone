0a1e607576762047c8b98c213dae96000a17af57
gregjbauer <gbauer1@mail.csuchico.edu>
2017-12-08 23:42:45 -0800

    ok

---
+++ player.h
@@ -0,0 +1,17 @@
+#include<string>
+#include<iostream>
+
+/*int main()
+{
+    private:
+        string pOneFirstName;
+        string pOneLastName;
+
+        string pTwoFirstName;
+        string pTwoFirstname;
+
+    public:
+        void setName(string, string);
+        void fullName(string, string);
+        char get
+}*/
