8dbc036b177943acb97cf1face51d5135bf424d6
gregjbauer <gbauer1@mail.csuchico.edu>
2017-12-08 23:41:53 -0800

    ok

---
+++ player.cpp
@@ -0,0 +1,45 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+/*void player::setNameOne(string first, string last)
+{
+    pOneFirstName = first;
+    pOneLastName = last;
+}
+
+void player::setNameTwo(string first, string last)
+{
+    pTwoFirstName = first;
+    pOneLastName = last;
+}
+
+void player::fullName(string pOneFirstName, string pTwoLastName)
+{
+        string fullOne;
+        fullOne.append(pOneFirstName);
+        fullOne.append(" ");
+        fullOne.append(pOneLastName);
+}
+
+void player::fullName(string pTwoFirstName, string pTwoLastName)
+{
+        string fullTwo;
+        fullTwo.append(pTwoFirstName);
+        fullTwo.append(" ");
+        fullTwo.append(pTwoLastName);
+}
+
+char player::placePiece(int placement)
+{
+    char playerOnePiece;
+    char playerTwoPiece;
+    
+    playerOnePiece = "1";
+    playerTwoPiece = "2";
+    
+return placement;
+}*/
+
+
